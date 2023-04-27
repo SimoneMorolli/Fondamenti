90ffc06d6d8c6a2c4d954aa3b9e064d2d892d817
SimoneMorolli <simone.morolli@gmail.com>
2023-04-27 14:20:56 +0200

    04-ShiftParametrico w/ main

--- Stringhe/04-ShiftParametrico/main.c
+++ Stringhe/04-ShiftParametrico/main.c
@@ -10,17 +10,14 @@ s = "abcdef" fornisce la stringa "defabc"*/
 #include <string.h>
 #include <stdlib.h>
 
-
-
-void shiftParametrico(char string[], int shiftNumber){
+void shiftParametrico(char string[], int shiftNumber, int leng){
 	
-	int lung = strlen(string), i;
+	int i;
 	char nextChar, actualChar;
-	printf("%s\n", string);
 
 	for (int c = 1; c <= shiftNumber; c++){
 	    actualChar = string[0];
-		for (i = 1; i < lung; i++){
+		for (i = 1; i < leng; i++){
 			nextChar = string[i];
 			string[i] = actualChar;
 			actualChar = nextChar;
@@ -31,16 +28,32 @@ void shiftParametrico(char string[], int shiftNumber){
 
 void testShiftParametrico(){
 	char string[] = "abcdef";
-	shiftParametrico(string, 0);
-	printf("Test shiftParametrico('abcdef', 0): Atteso = 'abcdef', Calcolato = '%s' \n", string);
+	shiftParametrico(string, 0, strlen(string));
+	printf("\nTest shiftParametrico('abcdef', 0): Atteso = 'abcdef', Calcolato = '%s' \n", string);
+
 	char string1[] = "abcdef";
-	shiftParametrico(string1, 2);
+	shiftParametrico(string1, 2, strlen(string1));
 	printf("Test shiftParametrico('abcdef', 2): Atteso = 'efabcd', Calcolato = '%s' \n", string1);
+	
 	char string2[] = "abcdef";
-	shiftParametrico(string2, 1);
+	shiftParametrico(string2, 1, strlen(string2));
 	printf("Test shiftParametrico('abcdef', 1): Atteso = 'fabcde', Calcolato = '%s' \n", string2);
 }
 
 int main(){
+	char string[50];
+	int x;
+	
+	printf("inserisci la bellissima stringa: ");
+	fgets(string, 50, stdin);
+	printf("inserisci il numero di volte che vuoi shiftare la stringa: ");
+	scanf("%d", &x);
+	
+	string[strlen(string - 1)] = '\0';
+	
+	shiftParametrico(string, x, strlen(string) - 1);
+
+	printf("la tua stringa shiftata di %d volte e': %s", x, string);
+
 	testShiftParametrico();
 }
