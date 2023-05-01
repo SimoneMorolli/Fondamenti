e5a9e7400f868bf911bf28437dbcb0f68628a713
Simone <simone.morolli@gmail.com>
2023-05-01 14:46:51 +0200

    Esonero2018-C Complete w/ main

--- Stringhe/EsameFebbraio2022/main.c
+++ Stringhe/EsameFebbraio2022/main.c
@@ -7,14 +7,11 @@ void xInRilievo(char string[]){
 
     for (int i = 0; i < leng;){
         if(string[i] != 'X'){
-            printf("%c e' diverso da X\n", string[i]);
             if(i != 0 && string[i + 1] == 'X'){
-                printf("Ho messo lo spazio\n");
                 string[i] = ' ';
                 i++;
             }
             else{
-                printf("sto shiftando la stringa\n");
                 for (int j = i; j < leng; j++)
                     string[j] = string[j + 1];
                 leng -= 1;
